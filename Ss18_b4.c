ab1e1276fe4ab037a80b781d2d9623cd2775fe28
AnHaiDung <anhaidung2k6@gmail.com>
2024-12-19 23:43:08 +0700

    b4

---
+++ Ss18_b4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+struct SinhVien{
+	char id[50];
+	char name[50];
+	int age;
+	char phoneNumber[15];
+};
+int main(){
+	struct SinhVien std1[50];
+		for(int i=0;i<5;i++){
+		    printf("nhap ten sinh vien: ");
+		    fgets(std1[i].name,sizeof(std1[i].name),stdin);
+		    printf("nhap tuoi sinh vien: ");
+		    scanf("%d",&std1[i].age);
+		    fflush(stdin);
+		    printf("nhap so dien thoai sinh vien: ");
+		    fgets(std1[i].phoneNumber,sizeof(std1[i].phoneNumber),stdin);
+	   };
+	for(int i=0;i<5;i++){
+		printf("id: %d\n",i+1);
+	    printf("ten sinh vien: %s\n",std1[i].name);
+		printf("tuoi sinh vien: %d\n",std1[i].age);
+		printf("so dien thoai sinh vien: %s\n",std1[i].phoneNumber);
+	}
+	return 0;
+}
